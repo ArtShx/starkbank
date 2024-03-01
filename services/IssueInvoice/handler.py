a737f8c36bc73fc870d0440d674e8e00ad9517a0
Arthur Miada <artshx@gmail.com>
2024-03-01 19:22:09 -0300

    Added pausing scheduler.

--- services/IssueInvoice/handler.py
+++ services/IssueInvoice/handler.py
@@ -4,9 +4,15 @@ Serverless function to issue invoices for random users.
 
 import functions_framework
 
+from datetime import datetime
+import os
 from random import randint
 
+from google.cloud import scheduler_v1
+from google.cloud.scheduler_v1.types import Job
+
 from starkbank_integration.auth import Authentication
+from starkbank_integration.environment import Environment
 from starkbank_integration.exceptions import ErrorCreatingInvoice
 from starkbank_integration.invoice import Invoice, InvoiceCreateRequest
 from starkbank_integration.db.base import BaseDB
@@ -23,8 +29,38 @@ def entrypoint(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
+    breakpoint()
+    if not should_run():
+        disable_scheduler()
     success = handler()
     return f"Success {success}!"
+
+
+def should_run() -> bool:
+    """
+    Checks if this job should be running now by looking at the Datastore configuration.
+    Maybe we should use something stored in Redis for efficience and more appropriate for this task.
+    """
+    db = BaseDB("IssueInvoiceConfig")
+    config = db.get_all(limit=1)[0]
+
+    end_time = config["end_time"]
+    # Converting DatetimeWithNanoSeconds to datetime
+    end_time = datetime.fromtimestamp(end_time.timestamp())
+    now = datetime.utcnow()
+    return now < end_time
+
+
+def disable_scheduler() -> bool:
+    """Pauses a scheduled job from GCP Cloud Scheduler."""
+    env_file = os.environ["env_file"]
+    env = Environment.from_file(env_file)
+    job_name = f"projects/{env['gcp_project']}/locations/{env['gcp_location']}/jobs/IssueInvoice"
+    client = scheduler_v1.CloudSchedulerClient()
+    request = scheduler_v1.PauseJobRequest(name=job_name)
+    response = client.pause_job(request=request)
+
+    return response.state == Job.State.PAUSED
 
 
 def handler():
@@ -49,4 +85,4 @@ def handler():
 
 
 if __name__ == "__main__":
-    handler()
+    entrypoint(None)
